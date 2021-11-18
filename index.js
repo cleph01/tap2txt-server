135d94009b7d8afe35e799c26b920e0973572c0e
Charlie <charles.j.montoya@gmail.com>
2021-11-18 15:15:10 -0500

    dot env setup and twilio messagin working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+
 const pino = require("express-pino-logger")();
 const client = require("twilio")(
     process.env.TWILIO_ACCOUNT_SID,
